15bf66537f688614a271fe1ce18d7e01070118ab
David Stancu <dstancu@nyu.edu>
2015-04-06 14:55:05 -0400

    better session management

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,30 @@ module ApplicationHelper
   def bootstrap_class_for(flash_level)
   	BOOTSTRAP_CLASSES[flash_level] || flash_level.to_s
   end
+
+  # Instead of including code for lookup under every view,
+  # we can just call this function by passing in the session
+  # object which will tell us all we need
+
+  def validate_user(session)
+    if session.has_key?("person")
+      user = Person.find_by_sql(
+        "SELECT clubadmin, superuser FROM person WHERE pid = '#{session[:person][:pid]}'")
+      p user
+      if user.empty?
+        :not_logged_in
+      else
+        if user.clubadmin
+          :clubadmin
+        elsif user.superuser
+          :superuser
+        else
+          :user
+        end
+      end
+    else
+      :not_logged_in
+    end
+  end
+
 end
